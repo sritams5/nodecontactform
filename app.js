c1ffa0cc9644e345e1c126eee15908a524b47c8a
Sritam Jagadev <sritams5@gmail.com>
2018-08-02 09:25:10 +0000

    mail send

--- app.js
+++ app.js
@@ -23,69 +23,71 @@ app.get('/', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-  const output = `
-    <p>You have a new contact request</p>
-    <h3>Contact Details</h3>
-    <ul>  
-      <li>Name: ${req.body.name}</li>
-      <li>Company: ${req.body.company}</li>
-      <li>Email: ${req.body.email}</li>
-      <li>Phone: ${req.body.phone}</li>
-    </ul>
-    <h3>Message</h3>
-    <p>${req.body.message}</p>
-  `;
+  var count= req.body.countText ;
+  var maillist=[];
+  for(var i=1;i<=count;i++){
+    maillist.push(req.body['mytext'+i]);
+  }
+  maillist.toString();
 
-  // create reusable transporter object using the default SMTP transport
-  // let transporter = nodemailer.createTransport({
-  //   host: 'mail.YOURDOMAIN.com',
-  //   port: 587,
-  //   secure: false, // true for 465, false for other ports
-  //   auth: {
-  //       user: 'YOUREMAIL', // generated ethereal user
-  //       pass: 'YOURPASSWORD'  // generated ethereal password
-  //   },
-  //   tls:{
-  //     rejectUnauthorized:false
-  //   }
-  // });
+//var mailid=req.body['mytext'+1];
+  // const output = `
+  //   <a href="https://www.google.com" />
+  // `;
+  console.log(maillist);
 
-  // setup email data with unicode symbols
-  // let mailOptions = {
-  //     from: '"Nodemailer Contact" <your@email.com>', // sender address
-  //     to: 'RECEIVEREMAILS', // list of receivers
-  //     subject: 'Node Contact Request', // Subject line
-  //     text: 'Hello world?', // plain text body
-  //     html: output // html body
-  // };
+  maillist.forEach(function (to, i , array) {
+    var transporter = nodemailer.createTransport(smtpTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    auth: {
+          user: 'slackmailing@gmail.com',
+          pass: 'Slack@246'
+      }
+    }));
+    const output = `
+      <a href="https://www.asdf.com?q='+to+'">Join</a>
+    `;
+    var msg = {
+      from: 'slackmailing@gmail.com', // sender address
+       to: maillist, // list of receivers
+       subject: 'Node Contact Request', // Subject line
+       text: 'Hello world?', // plain text body
+       html: output // html body
+      }
+    msg.to = to;
 
-  var transporter = nodemailer.createTransport(smtpTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
-  auth: {
-        user: 'slackmailing@gmail.com',
-        pass: 'Slack@246'
-    }
-}));
+    transporter.sendMail(msg, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        // console.log('Message sent: %s', info.messageId);
+        // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-const mailOptions = {
-  from: 'slackmailing@gmail.com', // sender address
-  to: 'sritams5@gmail.com', // list of receivers
-  subject: 'Node Contact Request', // Subject line
-  text: 'Hello world?', // plain text body
-  html: output // html body
-};
+    });
+  });
+
+  res.render('contact', {msg:'Email has been sent'});
+
+
+// const mailOptions = {
+//   from: 'slackmailing@gmail.com', // sender address
+//   to: maillist, // list of receivers
+//   subject: 'Node Contact Request', // Subject line
+//   text: 'Hello world?', // plain text body
+//   html: output // html body
+// };
 
   // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);   
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-      res.render('contact', {msg:'Email has been sent'});
+  // transporter.sendMail(mailOptions, (error, info) => {
+  //     if (error) {
+  //         return console.log(error);
+  //     }
+  //     console.log('Message sent: %s', info.messageId);
+  //     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  //
+  //     res.render('contact', {msg:'Email has been sent'});
+  // });
   });
-  });
 
-app.listen(3000, () => console.log('Server started...'));
+app.listen(8080, () => console.log('Server started...'));
